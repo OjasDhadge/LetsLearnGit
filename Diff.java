d39fe09af15648b4b6ea5e881d041195a2225bbd
Ojas Dhadge <ojasdhadge16@gmail.com>
2023-12-17 22:32:37 +0530

    adding Diff.java

---
+++ Diff.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Diff 
+{
+    public static void main(String[] args) 
+    {
+        int a,b,c;
+        a=100;
+        b=25;
+        c=a-b;
+        System.out.println("sum = "+c);
+    }
+    
+}
