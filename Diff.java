2629e1dfe83a7566eb87b23d8b2a79aacdf70028
Ojas Dhadge <ojasdhadge16@gmail.com>
2023-12-18 18:34:20 +0530

    added user input of nos in Diff.java

--- Diff.java
+++ Diff.java
@@ -1,13 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 public class Diff 
 {
     public static void main(String[] args) 
     {
         int a,b,c;
-        a=100;
-        b=25;
-        c=a-b;
-        System.out.println("sum = "+c);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a");
+        a=sc.nextInt();
+        System.out.println("enter b");
+        b = sc.nextInt();
+        c = a-b;
+        System.out.println("diff = "+c);
     }
     
 }
