bd8697f8b5cc0f4907ebeef9803c8de1e83365a0
Ojas Dhadge <ojasdhadge16@gmail.com>
2023-12-18 14:15:51 +0530

    changes made to div.java in dev

--- Div.java
+++ Div.java
@@ -3,6 +3,7 @@ public class Div {
         int a,b,c;
         a=100;b=20;
         c=a/b;
-        System.out.println("division is "+c);
+        System.out.println("the divison rersult  is  "+c);
     }
 }
+//changes made to be tracked by the git
